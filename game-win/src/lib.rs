68236350ec03ffdf320f9ef31b9c72631b6a88d5
Lut99 <mullertim99@gmail.com>
2022-03-28 21:06:52 +0200

    Added header

--- game-win/src/lib.rs
+++ game-win/src/lib.rs
@@ -1,8 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+/* LIB.rs
+ *   by Lut99
+ *
+ * Created:
+ *   28 Mar 2022, 21:06:39
+ * Last edited:
+ *   28 Mar 2022, 21:06:40
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Entrypoint to the crate that handles the windows for the game.
+**/
+
+
