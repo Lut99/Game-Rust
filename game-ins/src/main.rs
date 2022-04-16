a57818ac79d6abeffac3330cbc962bc9df5f6e9c
Lut99 <mullertim99@gmail.com>
2022-04-16 13:21:33 +0200

    Added setup executable
    However, probably will want to use FileHost (lib) for this

---
+++ game-ins/src/main.rs
@@ -0,0 +1,21 @@
+/* MAIN.rs
+ *   by Lut99
+ *
+ * Created:
+ *   16 Apr 2022, 13:13:03
+ * Last edited:
+ *   16 Apr 2022, 13:13:08
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Entrypoint to the tool that handles installing and/or deinstalling the
+ *   Game.
+**/
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
